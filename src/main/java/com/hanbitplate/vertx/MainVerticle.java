47d75ab0eb6d7a301d1344c12f106f1b29214536
Pahnki Kim <pahnki.kim.hanbit@gmail.com>
2016-12-20 11:43:21 +0900

    apply restart

--- src/main/java/com/hanbitplate/vertx/MainVerticle.java
+++ src/main/java/com/hanbitplate/vertx/MainVerticle.java
@@ -10,12 +10,29 @@ public class MainVerticle extends AbstractVerticle {
 	
 	public static void main(String[] args) {
 		Vertx vertx = Vertx.vertx();
-		vertx.deployVerticle(new MainVerticle());
+		
+		vertx.createHttpClient().getAbs("http://localhost:8080/shutdown",
+				response -> {
+					
+				})
+				.connectionHandler(connection -> {
+					connection.exceptionHandler(exception -> {
+						System.err.println("server stopped...");
+					});
+				})
+				.exceptionHandler(exception -> {
+					vertx.deployVerticle(new MainVerticle());
+					System.out.println("server started...");
+				}).end();
 	}
 
 	@Override
 	public void start() throws Exception {
 		Router router = Router.router(vertx);
+		
+		router.route("/shutdown").handler(ctx -> {
+			System.exit(0);
+		});
 		
 		router.route("/api/main/section/:sectionCode/items").handler(ctx -> {
 			HttpServerRequest request = ctx.request();
@@ -27,7 +44,41 @@ public class MainVerticle extends AbstractVerticle {
 			response.sendFile("json/section" + sectionCode + ".items.json");
 		});
 		
+		router.route("/api/main/section/:sectionCode/categories").handler(ctx -> {
+			HttpServerRequest request = ctx.request();
+			HttpServerResponse response = ctx.response();
+			
+			String sectionCode = request.getParam("sectionCode");
+			
+			response.putHeader("content-type", "application/json;charset=UTF-8");
+			response.sendFile("json/section" + sectionCode + ".categories.json");
+		});
+		
+		router.route("/api/common/hotplaces").handler(ctx -> {
+			HttpServerResponse response = ctx.response();
+			
+			System.out.println("hello");
+			
+			response.putHeader("content-type", "application/json;charset=UTF-8");
+			response.sendFile("json/common.hotplaces.json");
+		});
+		
 		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
